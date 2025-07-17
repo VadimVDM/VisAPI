376c6d06a3b609fabf19fd55b81063060828a90d
VadimVDM <vadim@vdm.digital>
2025-07-17 08:46:02 +0300

    fix: Trigger deployment to ensure email module is included

--- apps/backend/src/app/app.service.ts
+++ apps/backend/src/app/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getData(): { message: string } {
+    // Updated to trigger deployment with email module
     return { message: 'Hello API' };
   }
 }
